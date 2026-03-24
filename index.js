ccd2d744b8f9053d179efdd89652c78e5d07ac50
armstrong-khisa <armstrong.khisa@student.moringaschool.com>
2026-03-24 12:05:30 +0300

    learnt on hot to create objects and manipulate them in javascript also codded the index.js to manage event attendee

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 //Write your code here
+let attendees = [
+    { id: 1, name: "Alice", event: "Tech Conference", ticketType: "VIP", ticketPrice: 100 },
+    { id: 2, name: "Bob", event: "Music Festival", ticketType: "Regular", ticketPrice: 50 }
+];
 
+// Access
+console.log(attendees[0].name);
+
+// Add
+attendees.push({ id: 3, name: "Charlie", event: "Art Expo", ticketType: "VIP", ticketPrice: 80 });
+
+// Update
+attendees[1].ticketType = "VIP";
+attendees[1].ticketPrice = 75;
+
+// Remove
+attendees.splice(0, 1);
+
+// Final result
+console.log(attendees);
 
 
 //Needed for the tests to work. Don't modify
